5dc7b59399b4fa72913c4b4d232d517af39e9477
ye <15888520997@sina.cn>
2023-09-15 11:20:27 +0800

    添加日记记录

--- core/log.go
+++ core/log.go
@@ -15,7 +15,7 @@ func CmdLogs(logText string)bool{
 		return false
 	}
 	defer file.Close()
-	writeText := t.Format("2006-01-02 15:04:05")+"  "+logText+"\n"
+	writeText := t.Format("2006-01-02 15:04:05")+" --> "+logText+"\n"
 	if _, err := file.WriteString(writeText); err != nil {
 		log.Println("write file error: ",err.Error())
 		return false
