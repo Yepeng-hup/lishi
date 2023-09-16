48ce7ee4f77aeda6559e7558a6ad393efbdc6d2b
ye <15888520997@sina.cn>
2023-09-16 16:30:13 +0800

    更新内存获取bug

--- cmd/memory.go
+++ cmd/memory.go
@@ -4,13 +4,29 @@ import (
 	"github.com/shirou/gopsutil/mem"
 )
 
+const gm uint64 = 1074000000
 
 func (s *System) MemSy() *MemInfo {
 	m, _ := mem.VirtualMemory()
-	mems := &MemInfo{
-		MemTotal: float64(m.Total/1024/1024/1024),
-		MemUsed: float64(m.Active/1024/1024/1024),
-		MemFree: float64(m.Free/1024/1024/1024),
+	if m.Total < gm {
+		total := float64(m.Total/1024/1024) / float64(1024)
+		active := float64(m.Active/1024/1024) / float64(1024)
+		free := float64(m.Free/1024/1024) / float64(1024)
+		mems := &MemInfo{
+			MemTotal: total,
+			MemUsed: active,
+			MemFree: free,
+		}
+		return mems
+	}else {
+		total := float64(m.Total/1024/1024/1024)
+		active := float64(m.Active/1024/1024/1024)
+		free := float64(m.Free/1024/1024/1024)
+		mems := &MemInfo{
+			MemTotal: total,
+			MemUsed: active,
+			MemFree: free,
+		}
+		return mems
 	}
-	return mems
 }
